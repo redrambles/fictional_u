dfeb8380fc3570edece4b513c88dcb8f7ead488f
Ann Cascarano <a.cascarano@gmail.com>
2017-11-01 22:29:00 -0400

    add university-post-type

---
+++ wp-content/mu-plugins/university-post-types.php
@@ -0,0 +1,15 @@
+<?php
+
+function university_post_types(){
+  register_post_type( 'event', array(
+      'public' => true,
+      'labels' => array(
+        'name' => 'Events'
+      ),
+      'menu_icon' => 'dashicons-calendar'
+    )
+  );
+}
+
+add_action('init', 'university_post_types');
+
