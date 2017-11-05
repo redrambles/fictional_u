7ae9d7c814b8087ac32c787b939fe9117cc290a1
Ann Cascarano <a.cascarano@gmail.com>
2017-11-05 10:22:40 -0500

    add to .gitignore

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-// Using MAMP
+// Using MAMP - run this setup using 'gulp watch'
 
 exports.themeLocation = './wp-content/themes/fictional-university-theme/';
 exports.urlToPreview = 'http://localhost/fictional_university';
