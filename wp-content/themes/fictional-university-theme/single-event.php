072d032e022ac4ee51f19345e3e3b9f80025fa09
Ann Cascarano <a.cascarano@gmail.com>
2017-11-04 23:57:02 -0400

    add relationship between programs and events

--- wp-content/themes/fictional-university-theme/single-event.php
+++ wp-content/themes/fictional-university-theme/single-event.php
@@ -21,6 +21,19 @@
 
       <div class="generic-content"><?php the_content(); ?></div>
 
+      <?php
+        $relatedPrograms = get_field('related_program');
+
+        if ($relatedPrograms) { 
+          echo '<hr class="section-break">';
+          echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+          echo '<ul class="link-list min-list">';
+          foreach( $relatedPrograms as $program ){ 
+            echo '<li><a href="' . get_the_permalink($program) . '">' . get_the_title($program) . '</a></li>';
+          } 
+            echo '</ul>';
+        } ?>
+
     </div>
     
 
--- wp-content/themes/fictional-university-theme/single-program.php
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -21,9 +21,53 @@
 
       <div class="generic-content"><?php the_content(); ?></div>
 
-    </div>
-    
+      <?php
+          $args = array(
+            'posts_per_page' => -1,
+            'post_type' => 'event',
+            'meta_key' => 'event_date',
+            'orderby' => 'meta_value_num',
+            'order' => 'ASC',
+            'meta_query' => array(
+              array(
+                'key' => 'related_program',
+                'compare' => 'LIKE',
+                'value' => '"' . get_the_ID() . '"' // to handle the serialization of the data
+              )
+            )
+          );
+          $homepageEvents = new WP_Query($args);
 
+          if ( $homepageEvents->have_posts() ) :
+            echo '<hr class="section-break">';
+            echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
+
+            while($homepageEvents->have_posts() ) : $homepageEvents->the_post(); 
+              $eventDate = new DateTime( get_field('event_date') );
+              $today = new DateTime();
+              if( $eventDate >= $today ) : ?>
+                <div class="event-summary">
+                  <a class="event-summary__date t-center" href="#">
+                    <span class="event-summary__month">
+                    <?php 
+                    echo $eventDate->format('M'); 
+                    ?></span>
+                    <span class="event-summary__day"><?php echo $eventDate->format('j'); ?></span>  
+                  </a>
+                  <div class="event-summary__content">
+                  <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                  <p> <?php if( has_excerpt() ) { 
+                        the_excerpt(); } else { 
+                          echo wp_trim_words(get_the_content(), 18);
+                        } ?> 
+                    <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                  </div>
+                </div>
+            <?php endif; endwhile;
+              wp_reset_postdata();  
+              endif;
+           ?>
+    </div>
     
   <?php }
 
