3edfc2739b036e9b7b50aa4a47f48b191c1e30c3
Ann Cascarano <a.cascarano@gmail.com>
2017-11-09 19:37:57 -0500

    abstracting events structure to template part content-event

--- wp-content/themes/fictional-university-theme/single-program.php
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -52,6 +52,7 @@
 
         <?php
           // Related Events
+          $today = date('Ymd');
           $args = array(
             'posts_per_page' => -1,
             'post_type' => 'event',
@@ -63,40 +64,26 @@
                 'key' => 'related_program',
                 'compare' => 'LIKE',
                 'value' => '"' . get_the_ID() . '"' // to handle the serialization of the data
+              ),
+              array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
               )
             )
           );
           $homepageEvents = new WP_Query($args);
-          $counter = 0;
 
           if ( $homepageEvents->have_posts() ) :
             echo '<hr class="section-break">';
             echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
 
-            while($homepageEvents->have_posts() && ($counter < 2) ) : $homepageEvents->the_post(); 
+            while($homepageEvents->have_posts() ) : $homepageEvents->the_post(); 
               $eventDate = new DateTime( get_field('event_date') );
-              $today = new DateTime();
-              if( $eventDate >= $today ) : ?>
-                <div class="event-summary">
-                  <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month">
-                    <?php 
-                    echo $eventDate->format('M'); 
-                    ?></span>
-                    <span class="event-summary__day"><?php echo $eventDate->format('j'); ?></span>  
-                  </a>
-                  <div class="event-summary__content">
-                  <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                  <p> <?php if( has_excerpt() ) { 
-                        the_excerpt(); } else { 
-                          echo wp_trim_words(get_the_content(), 18);
-                        } ?> 
-                    <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-                  </div>
-                </div>
-            <?php $counter++; 
-                  endif; 
-                endwhile;  
+              $today = new DateTime(); 
+                get_template_part('template-parts/content', 'event');
+              endwhile;  
               endif;
             wp_reset_postdata(); 
            ?>
