bdfc8a36bc24385a6aebb13f09c07ba4604e68dd
Ann Cascarano <a.cascarano@gmail.com>
2018-03-20 21:12:44 -0400

    update to 4.9.4 and update akismet

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -10,32 +10,34 @@
     <div class="container container--narrow page-section">
     
     <?php
+      // Grab the Parent Page ID, if it exists (so if this is a child page)
       $theParent = wp_get_post_parent_id(get_the_ID());
       if ($theParent) { ?>
         <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
-    </div>
+          <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+        </div>
       <?php }
-    ?>
 
-    
-    
-    <?php 
+    // is this a parent page? If so - grab ids of child pages
     $testArray = get_pages(array(
       'child_of' => get_the_ID()
     ));
 
-    if ($theParent or $testArray) { ?>
+    // If this is a child page or if this is a parent page
+    if ($theParent or $testArray) { 
+      
+    // if either of these conditions are true, there is at the very least a parent page, so let's output that ?>
     <div class="page-links">
       <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
       <ul class="min-list">
         <?php
+          // Does this page have a parent page? If so, set the $findChildrenOf variable to the parent page id
           if ($theParent) {
             $findChildrenOf = $theParent;
-          } else {
+          } else { // otherwise, set the $findChildrenOf variable to this current page, because it is a parent page.
             $findChildrenOf = get_the_ID();
           }
-
+          // whether this is a parent or a child page, the related child pages will show (either as children or siblings)
           wp_list_pages(array(
             'title_li' => NULL,
             'child_of' => $findChildrenOf,
